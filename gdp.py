d3183b88118ea383fb19559c894b2a1a8e4f3425
CristopherMasserini <crismasserini@gmail.com>
2024-09-16 17:13:24 -0400

    Clean Columns

--- gdp.py
+++ gdp.py
@@ -5,7 +5,9 @@ GDP data can be found: https://bea.gov/itable/national-gdp-and-personal-income
 import pandas as pd
 
 data = pd.read_csv('Data/GDP_data.csv')
-print(data.columns)
-print(data.head())
 
-# Remove: Change in private inventories, Net exports of goods and services, Addendum
+# Remove: Change in private inventories, Net exports of goods and services, Addendum:
+df = data.drop(columns='Change in private inventories')
+df = df.drop(columns='Net exports of goods and services')
+df = df.drop(columns='Addendum:')
+print(df.columns)
