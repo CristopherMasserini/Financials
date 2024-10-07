23b15827b41689af0a8d30f30d0a4ca1c1d5b197
CristopherMasserini <crismasserini@gmail.com>
2024-10-07 16:57:58 -0400

    Concat years and quarters

--- gdp.py
+++ gdp.py
@@ -13,10 +13,25 @@ def read_data():
     df = data.drop(columns='Change in private inventories')
     df = df.drop(columns='Net exports of goods and services')
     df = df.drop(columns='Addendum:')
+
     return df
 
 
+def concat_year_quarter(df):
+    df['Year Quarter'] = df['Year'].astype(str) + df['Quarter'].astype(str)
+    return df
+
 def plot_columns(df, col1, col2):
-    plt.plot(col1, col2)
+    x = df.loc[:, col1]
+    y = df.loc[:, col2]
+    plt.plot(x, y)
     plt.show()
 
+
+def run():
+    df = read_data()
+    concat_year_quarter(df)
+    # print(concat_year_quarter(df))
+    # plot_columns(df, 'Year', 'Personal consumption expenditures')
+
+run()
