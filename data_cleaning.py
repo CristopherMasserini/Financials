8e715d89b3fa8ab5ee35a26eb30ba7b3e53c5817
CristopherMasserini <crismasserini@gmail.com>
2024-10-25 15:57:47 -0400

    Data cleaning and trailing quarters

--- data_cleaning.py
+++ data_cleaning.py
@@ -32,7 +32,27 @@ def save_dataframe(df):
     df.to_csv('Data/GDP_data_cleaned.csv', index=False)
 
 
+def trailing(df):
+    # Trailing average of previous 4 quarters
+    col = 'Personal consumption expenditures'
+    col_data = df.loc[:, col]
+    dataNew = {'Year Quarter': df.loc[4:, 'Year Quarter'],
+               'GDP': df.loc[4:, 'Gross domestic product'],
+               col: []}
+
+    init_trail_range = [0, 1, 2, 3]
+    for i in range(0, len(dataNew['Year Quarter'])):
+        sum = 0
+        for j in init_trail_range:
+            sum += col_data[j]
+        dataNew[col].append(sum / 4)
+
+        init_trail_range = [x + 1 for x in init_trail_range]
+
+    return dataNew
+
+
 if __name__ == '__main__':
-    df = read_data()
-    df = clean_data(df)
-    save_dataframe(df)
+    data = read_data()
+    data = clean_data(data)
+    save_dataframe(data)
