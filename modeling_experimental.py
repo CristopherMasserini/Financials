3ed48493d20f76e6bfe53d2b3cff3be34e90bcf9
Cristopher Masserini <61479774+CristopherMasserini@users.noreply.github.com>
2025-02-25 17:01:27 -0500

    PKNN modeling

--- modeling_experimental.py
+++ modeling_experimental.py
@@ -1,13 +1,12 @@
 # Assumes the submodule https://github.com/CristopherMasserini/PenalizedKNN.git is added.
-from PenalizedKNN.Data import DataSet
-from PenalizedKNN.Model import KNNPenalized
+from PenalizedKNN import Data, Model
 
 
 def modeling(file, nclassifiers):
-    dataset = DataSet()
-    model = KNNPenalized(nclassifiers)
+    dataset = Data.DataSet()
+    model = Model.KNNPenalized(nclassifiers)
     dataset.dataframe_to_dataset(file, 'Label')
-    model.test_model(dataset, 0.2, 'Label')
+    model.test_model(dataset, 0.2, ["0", "1", "2", "3"])
 
 
 if __name__ == '__main__':
